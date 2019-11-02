ce1a7387b9a93330e0624396a73c8eaec7bd2be4
vitorlorente <lorente.sociologia@gmail.com>
2019-11-01 22:43:31 -0300

    Arquivo para gerar tabelas e carga inicial de banco de dados finalizado.

--- migrate.py
+++ migrate.py
@@ -14,27 +14,35 @@ try:
         id serial PRIMARY KEY,
         name varchar (50) NOT NULL,
         email varchar (50) NOT NULL,
-        CONSTRAINT UNIQUE (email)
+        CONSTRAINT unique_customer_email UNIQUE (email)
         );""")
 
     # Produto
     cursor.execute("""CREATE TABLE Product(
         id serial PRIMARY KEY,
-        title varchar (30) NOT NULL,
+        title varchar (100) NOT NULL,
         brand varchar (30) NOT NULL,
-        image varchar (30) NOT NULL,
+        image varchar (100) NOT NULL,
         price float (2) NOT NULL,
-        reviewScore float(2) NOT NULL,
+        reviewScore float(2) NOT NULL
         );""")
 
     # Criando 10 clientes iniciais
-    for i in range(1, 10):
-        cursor.execute(f"INSERT INTO Customer(name, email) VALUES ('Cliente {i}', 'cliente{i}@luizalabs.com');")
+    for i in range(0, 1000):
+        cursor.execute(f"INSERT INTO Customer(name, email) VALUES ('Cliente {i+1}', 'cliente{i+1}@luizalabs.com');")
 
     # Criando 5 produtos
     cursor.execute("""INSERT INTO Product(title, brand, image, price, reviewScore)
     VALUES ('TV 29 polegadas', 'Samsung', 'imgs/tvs/samsung/29-polegadas.jpeg', 1000.00, 9.50),
-    ('iPhone 8 256GB Prata', 'apple', 'imgs/smartphones/apple/iphone8-256-prata.jpeg', 3149.91, 9.25);""")
+    ('iPhone 8 256GB Prata', 'Apple', 'imgs/smartphones/apple/iphone8-256-prata.jpeg', 3149.91, 9.00),
+    ('Relógio Feminino Mondaine Analógico', 'Mondaine', 'imgs/relogios/mondaine/relógio-feminino-mondaine-analógico.jpeg', 132.90, 10.00),
+    ('Faqueiro Inox Búzios 72 Peças', 'Tramontina', 'imgs/faqueiros/tramontina/faqueiro-inox-buzios-72-peças.jpeg', 109.00, 9.25),
+    ('Papel Higiênico Folha Dupla', 'Neve', 'imgs/papel-higienico/neve/papel-higienico-folha-dupla.jpeg', 17.90, 9.25),
+    ('Cômoda Infantil 2 Gavetas', 'Art In Móveis', 'imgs/comoda/art-in-moveis/comoda-infantil-2-gavetas.jpeg', 377.06, 9.80),
+    ('Aparelho de Barbear', 'Gillette', 'imgs/aparelho-barbear/gillete/aparelho-de-barbear.jpeg', 112.20, 10.00),
+    ('Loção Pós-Barba Gillette Sensitive Skin', 'Gillette', 'imgs/locao-pos-barba/gillete/locao-pos-barba.jpeg', 29.90, 9.90),
+    ('Ferro de Passar a Vapor e a Seco', 'Arno', 'imgs/ferro-de-passar/arno/ferro-de-passar-a-vapor-e-a-seco.jpeg', 151.91, 7.00),
+    ('Cabeceira Solteiro', 'Madesa', 'imgs/cabeceira-solteiro/madesa/cabeceira-solteiro.jpeg', 100.71, 6.0);""")
 
     connection.commit()
 
